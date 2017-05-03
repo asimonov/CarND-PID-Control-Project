e57268489ec2ffa1a70f68e94ae67b0a64b6b931
Alex <alexey.simonov@gmail.com>
2017-05-03 10:46:13 +0100

    doing great at 55mph

--- src/main.cpp
+++ src/main.cpp
@@ -46,13 +46,13 @@ int main()
 
   // Initialize the PID controller.
   PID pid;
-  double init_Kp = 0.5;
-  double init_Ki = 0.2;
-  double init_Kd = 7.0;
+  double init_Kp = 0.55;
+  double init_Ki = 0.15;
+  double init_Kd = 8.0;
   static unsigned int integral_length = 5;
   //static unsigned int twiddle_length = 5660; // this is when to optimize twiddle. every lap
   //static unsigned int twiddle_length = 3800; // this is when to optimize twiddle. every lap
-  static unsigned int twiddle_length = 1600; // this is when to optimize twiddle. every lap
+  static unsigned int twiddle_length = 1400; // this is when to optimize twiddle. every lap
   pid.Init(init_Kp, init_Ki, init_Kd, integral_length, twiddle_length);
 
 
@@ -130,7 +130,7 @@ int main()
 
           //out_throttle = 0.1;
           //out_throttle = 0.15;
-          out_throttle = 0.4;
+          out_throttle = 0.5;
           // DEBUG
           std::cout << "IN CTE: " << in_cte << " Total Error: " << pid.TotalError() << " Steering Value: " << out_steering << std::endl;
 
