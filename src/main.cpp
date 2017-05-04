f5cf0480946b70bc49b3cd72da6f21fa0390d78d
Alex <alexey.simonov@gmail.com>
2017-05-04 10:32:56 +0100

    goes between 50 and 60. using brakes from time to time

--- src/main.cpp
+++ src/main.cpp
@@ -46,9 +46,9 @@ int main()
 
   // Initialize the PID controller.
   PID pid;
-  static double init_Kp = 0.65;
-  static double init_Ki = 0.2;
-  static double init_Kd = 11.0;
+  static double init_Kp = 1.0;
+  static double init_Ki = 0.1;
+  static double init_Kd = 9.0;
   static unsigned int integral_length = 2;
   //static unsigned int twiddle_length = 5660; // this is when to optimize twiddle. every lap
   //static unsigned int twiddle_length = 3800; // this is when to optimize twiddle. every lap
@@ -121,22 +121,33 @@ int main()
           * another PID controller to control the speed!
           */
 
-          if (frame > 100 && in_speed > 65.)
-            out_throttle = 0.1;
+          if (in_speed > 70.)
+            out_throttle = -1.0;
           else
             out_throttle = 1.0;
+
+          if (in_cte < 1.)
+            pid.Kp_ = init_Kp * 0.5;
+          else if (in_cte < 2.)
+            pid.Kp_ = init_Kp * 0.8;
+          else
+          {
+            pid.Kp_ = init_Kp * 1.0;
+            out_throttle = -1.0;
+          }
 
           out_steering = pid.PredictSteering(in_cte, in_speed, dt);
 
-          if (frame > 100 && (abs(out_steering) > 0.2))
-            out_throttle = 0.1;
-          else
-            out_throttle = 1.0;
+          if (frame > 100 && (abs(out_steering) > 0.1))
+            out_throttle = 0.0;
+//          else
+//            out_throttle = 1.0;
 
           if (out_steering < -1.)
             out_steering = -1.;
           else if (out_steering > 1.)
             out_steering = 1.;
+
           pid.UpdateError(in_cte, in_speed, dt);
           //pid.TwiddleIfEnoughHistory();
 
